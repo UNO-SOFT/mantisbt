fcc2f20cfe99b8dbca9562b1691a5134d637017a
Tamás Gulácsi <tamas@gulacsi.eu>
2021-03-17 10:16:39 +0100

    new statuses

--- config/config_statuses_inc.php
+++ config/config_statuses_inc.php
@@ -1,16 +1,258 @@
 <?php
-if(!defined('NEW_')) define('NEW_', 10);
-if(!defined('CLOSED')) define('CLOSED', 999);
+/*
+if(!defined('NEW_')) define('NEW_', 10);  // uj
+if(!defined('FEEDBACK')) define('FEEDBACK', 20);  // kerdes
+if(!defined('ASK_PROPOSAL')) define('ASK_PROPOSAL', 25);  // ajanlat keres
+if(!defined('PROPOSAL')) define('PROPOSAL', 30);  // ajanlat
+if(!defined('ACKNOWLEDGED')) define('ACKNOWLEDGED', 40);  // elfogadva
+if(!defined('ASSIGNED')) define('ASSIGNED', 50);  // folyamatban
+if(!defined('TEST')) define('TEST', 60);  // belso teszt
+if(!defined('TEST_OK')) define('TEST_OK', 70);  // belso teszt ok
+if(!defined('RESOLVED')) define('RESOLVED', 80);  // atadva
+if(!defined('CLOSED')) define('CLOSED', 90);  // lezarva
+if(!defined('STORNO')) define('STORNO', 100);
+ */
 
 require_once(dirname(__FILE__) . '/../config_defaults_inc.php');
 
+$g_status_enum_workflow[10] = '20:feedback,50:assigned,90:closed';
+$g_status_enum_workflow[20] = '25:ask_proposal,50:assigned,90:closed';
+$g_status_enum_workflow[25] = '30:proposal,20:feedback,90:closed';
+$g_status_enum_workflow[30] = '40:acknowledged,90:closed';
+$g_status_enum_workflow[40] = '50:assigned,90:closed';
+$g_status_enum_workflow[50] = '20:feedback,60:test,80:resolved';
+$g_status_enum_workflow[60] = '50:assigned,70:test_ok';
+$g_status_enum_workflow[70] = '80:resolved';
+$g_status_enum_workflow[80] = '50:assigned,90:closed';
+$g_status_enum_workflow[90] = '';
+
+$g_status_enum_string = '10:new,20:feedback,30:proposal,40:acknowledged,50:assigned,60:test,70:test_ok,80:resolved,90:closed';
+
+$g_set_status_threshold = array (
+	10 => REPORTER,
+	20 => UPDATER,
+	25 => REPORTER,
+	30 => MANAGER,
+	40 => REPORTER,
+	50 => UPDATER,
+	60 => DEVELOPER,
+	70 => UPDATER,
+	80 => DEVELOPER,
+	90 => DEVELOPER	
+);
+
+$g_status_colors = array(
+	'new' => '#FF9999',
+	'feedback' => '#FFED75',
+	'ask_proposal' => '#FFED75',
+	'proposal' => '#F9FFA7',
+	'acknowledged' => '#ABEDEC',
+	'assigned' => '#ABEDEC',
+	'test' => '#CAFD8A',
+	'test_ok' => '#9CE964',
+	'resolved' => '#D9D9D9',
+	'closed' => '#FFFFFF',
+);
+
+/*
+
+ digraph Mantis {
+	NEW_ [label="új" color="green"];
+	FEEDBACK [label="kérdés" color="red" shape="rectangle"];
+	ASSIGNED [label="folyamatban"];
+	RESOLVED [label="átadva" color="red" shape="rectangle"];
+	CLOSED [label="lezárva"];
+	TEST [label="belső teszt"];
+	TEST_OK [label="belső teszt ok"];
+	ASK_PROPOSAL [label="ajánlat kérés"];
+	PROPOSAL [label="ajánlat" color="red" shape="rectangle"];
+	ACCEPTED [label="elfogadva" color="green"];
+
+	NEW_ -> ASK_PROPOSAL -> PROPOSAL -> ACCEPTED -> ASSIGNED;
+	ASK_PROPOSAL -> FEEDBACK -> ASK_PROPOSAL;
+	FEEDBACK -> PROPOSAL -> CLOSED;
+
+	NEW_ -> FEEDBACK -> ASSIGNED -> FEEDBACK -> CLOSED;
+	NEW_ -> ASSIGNED -> RESOLVED -> CLOSED [weight=10];
+	ASSIGNED -> TEST -> TEST_OK -> RESOLVED -> ASSIGNED;
+	TEST -> ASSIGNED;
+}
+
+BEGIN;
+
+CREATE TABLE mantis_bug_table_20210307 AS SELECT * FROM mantis_bug_table;
+
+UPDATE mantis_bug_table A
+    SET projection = (SELECT CASE C.value WHEN 'hibajavítás' THEN 20  WHEN 'Hibabejelentés' THEN 20
+    WHEN 'megrendelés' THEN 50 WHEN 'Fejlesztés' THEN 50
+    WHEN 'SPL-Fejlesztés' THEN 51 WHEN 'Lekérdezés' THEN 52 WHEN 'SPOOLSYS' THEN 51 WHEN 'oktatás' THEN 53 
+    WHEN 'konzultáció' THEN 55 WHEN 'Konzultáció' THEN 55
+	ELSE 10 END 
+	FROM mantis_custom_field_string_table C, mantis_custom_field_table B 
+	WHERE C.bug_id = A.id AND C.field_id = B.id AND B.name LIKE 't_pus')
+	WHERE EXISTS (SELECT 1 FROM mantis_custom_field_string_table C, mantis_custom_field_table B 
+          WHERE C.bug_id = A.id AND C.value <> '' AND C.field_id = B.id AND B.name LIKE 't_pus');
+
+UPDATE mantis_bug_table
+  SET projection = CASE MOD(status, 10) WHEN 1 THEN 20 WHEN 2 THEN 50 ELSE 10 END
+  WHERE status >= 100 AND MOD(status, 10) BETWEEN 1 AND 3;
+
+UPDATE mantis_bug_table A
+ SET projection = CASE (SELECT MOD(MAX(TO_NUMBER(new_value,'999')), 10) FROM mantis_bug_history_table  B
+WHERE field_name = 'status' AND B.bug_id = A.id AND TO_NUMBER(new_Value, '999')<999)
+   WHEN 2 THEN 50 ELSE 10 END
+  WHERE EXISTS (SELECT 1 FROM mantis_bug_history_table B
+WHERE field_name = 'status' AND B.bug_id = A.id AND TO_NUMBER(new_Value, '999')<999) AND
+        status >= 100 AND projection = 10;
+
+UPDATE mantis_bug_table
+ SET status = CASE status
+ WHEN 666 THEN 50
+ WHEN 101 THEN 10 WHEN 102 THEN 10 WHEN 103 THEN 10
+ WHEN 201 THEN 50 WHEN 202 THEN 30 WHEN 203 THEN 50
+ WHEN 301 THEN 50 WHEN 302 THEN 25 WHEN 303 THEN 50
+ WHEN 401 THEN 50 WHEN 402 THEN 40 WHEN 403 THEN 50
+ WHEN 501 THEN 50 WHEN 502 THEN 50 WHEN 503 THEN 50
+ WHEN 701 THEN 60 WHEN 702 THEN 60 WHEN 703 THEN 60
+ WHEN 801 THEN 70 WHEN 802 THEN 70 WHEN 803 THEN 70
+ WHEN 901 THEN 60 WHEN 902 THEN 60 WHEN 903 THEN 60
+ WHEN 981 THEN 50 WHEN 982 THEN 50 WHEN 983 THEN 50
+ WHEN 991 THEN 80 WHEN 992 THEN 80 WHEN 993 THEN 80
+ WHEN 999 THEN 90
+ ELSE 50
+ END
+ WHERE status >= 100;
+
+
+UPDATE mantis_bug_history_table
+ SET old_value = CASE old_value
+ WHEN '666' THEN '50'
+ WHEN '101' THEN '10' WHEN '102' THEN '10' WHEN '103' THEN '10'
+ WHEN '201' THEN '50' WHEN '202' THEN '30' WHEN '203' THEN '50'
+ WHEN '301' THEN '50' WHEN '302' THEN '25' WHEN '303' THEN '50'
+ WHEN '401' THEN '50' WHEN '402' THEN '40' WHEN '403' THEN '50'
+ WHEN '501' THEN '50' WHEN '502' THEN '50' WHEN '503' THEN '50'
+ WHEN '701' THEN '60' WHEN '702' THEN '60' WHEN '703' THEN '60'
+ WHEN '801' THEN '70' WHEN '802' THEN '70' WHEN '803' THEN '70'
+ WHEN '901' THEN '60' WHEN '902' THEN '60' WHEN '903' THEN '60'
+ WHEN '981' THEN '50' WHEN '982' THEN '50' WHEN '983' THEN '50'
+ WHEN '991' THEN '80' WHEN '992' THEN '80' WHEN '993' THEN '80'
+ WHEN '999' THEN '90'
+ ELSE old_value
+ END
+ WHERE field_name = 'status' AND old_value >= '100' AND old_value IS NOT NULL;
+
+UPDATE mantis_bug_history_table
+ SET new_value = CASE new_value
+ WHEN '666' THEN '50'
+ WHEN '101' THEN '10' WHEN '102' THEN '10' WHEN '103' THEN '10'
+ WHEN '201' THEN '50' WHEN '202' THEN '30' WHEN '203' THEN '50'
+ WHEN '301' THEN '50' WHEN '302' THEN '25' WHEN '303' THEN '50'
+ WHEN '401' THEN '50' WHEN '402' THEN '40' WHEN '403' THEN '50'
+ WHEN '501' THEN '50' WHEN '502' THEN '50' WHEN '503' THEN '50'
+ WHEN '701' THEN '60' WHEN '702' THEN '60' WHEN '703' THEN '60'
+ WHEN '801' THEN '70' WHEN '802' THEN '70' WHEN '803' THEN '70'
+ WHEN '901' THEN '60' WHEN '902' THEN '60' WHEN '903' THEN '60'
+ WHEN '981' THEN '50' WHEN '982' THEN '50' WHEN '983' THEN '50'
+ WHEN '991' THEN '80' WHEN '992' THEN '80' WHEN '993' THEN '80'
+ WHEN '999' THEN '90'
+ ELSE old_value
+ END
+ WHERE field_name = 'status' AND new_value >= '100' AND new_value IS NOT NULL;
+
+
+*/
+
+//$g_reproducibility_enum_string = '10:always,30:sometimes,50:random,70:have not tried,90:unable to duplicate,100:N/A';
+//$g_status_enum_string = '10:new,20:feedback,30:acknowledged,40:confirmed,50:assigned,80:resolved,90:closed';
+
+$g_enable_projection = ON;
+//$g_projection_enum_string = '10:none,30:tweak,50:minor fix,70:major rework,90:redesign';
+$g_projection_enum_string = '10:support,20:error,50:order';
+
+//$g_resolution_enum_string = '10:open,20:fixed,30:reopened,40:unable to duplicate,50:not fixable,60:duplicate,70:not a bug,80:suspended,90:wont fix';
+$g_resolution_enum_string = '10:open,20:fixed,21:storno,29:executed,30:reopened,40:unable to duplicate,50:not fixable,60:duplicate,70:not a bug,80:suspended,90:wont fix';
+
+//$g_severity_enum_string = '10:feature,20:trivial,30:text,40:tweak,50:minor,60:major,70:crash,80:block';
+$g_severity_enum_string	= '10:feature,50:error';
+
+//$g_priority_enum_string = '10:none,20:low,30:normal,40:high,50:urgent,60:immediate';
+$g_priority_enum_string	= '30:normal,40:high,60:immediate';
+
+
+$g_default_notify_flags = array(
+	'reporter' => ON,
+	'handler' => OFF,
+	'monitor' => ON,
+	'bugnotes' => OFF,
+	//'threshold_min' => REPORTER,
+	//'threshold_max' => REPORTER,
+);
+if (! $g_notify_flags ) $g_notify_flags = $g_default_notify_flags;
+$g_notify_flags = array_merge_recursive( $g_notify_flags, array (
+	'reopened' => array (
+		'reporter' => ON,
+		'handler' => ON,
+		'monitor' => ON,
+	),
+	'deleted' => array (
+		'reporter' => ON,
+		'handler' => ON,
+		'monitor' => ON,
+	),
+	'bugnote' => array (
+		'reporter' => ON,
+		'monitor' => ON,
+		'handler' => ON,
+		//'threshold_max' => REPORTER,
+	),
+) );
+
+$g_auto_set_status_to_assigned = OFF;
+
+$g_allow_reporter_reopen = OFF;
+$g_allow_reporter_close = ON;
+
+$g_use_persistent_connections = ON;
+
+$g_relationship_graph_enable = ON;
+
+$g_default_email_on_new = OFF;
+$g_default_email_on_assigned = OFF;
+$g_default_email_on_feedback = OFF;
+$g_default_email_on_resolved = OFF;
+$g_default_email_on_closed = OFF;
+$g_default_email_on_reopened = OFF;
+$g_default_email_on_bugnote = ON;
+
+$g_hide_status_default = CLOSED;
+$g_hide_status = CLOSED;
+$g_bug_resolved_status_threshold = RESOLVED;
+$g_bug_readonly_status_threshold = RESOLVED;
+$g_update_readonly_bug_threshold = DEVELOPER;
+$g_private_bug_threshold = UPDATER;
+$g_private_bugnote_threshold = UPDATER;
+$g_update_bug_status_threshold = REPORTER;
+$g_set_view_status_threshold = UPDATER;
+$g_change_view_status_threshold = UPDATER;
+
+$g_bug_reopen_status = 50;
+$g_bug_closed_status_threshold = 90;
+
+$g_bug_status_modulo = 1;
+$g_bug_status_modulo_permanent = ON;
+
+require_once(dirname(__FILE__) . '/custom_strings_inc.php');
+
+
+/*
 define('JOKER', 666);
 
 define('X_ERR', 101);
 define('X_ORD', 102);
 define('X_SUP', 103);
 $g_status_enum_workflow[NEW_] =
-  '101:new error,102:new order,103:support,999:closed,666:joker';
+	'101:new error,102:new order,103:support,999:closed,666:joker';
 
 define('E_FEEDBACK', 201);
 define('E_EXECUTE', 301);
@@ -72,71 +314,71 @@ $g_status_enum_workflow[999] = '666:joker';
 $g_status_enum_string = '10:new,101:new error,102:new order,103:new support,201:error feedback,301:error execute,701:error inner test,801:error inner test ok,901:error test,981:error confirmed,991:fixed,202:proposal,302:ask proposal,402:accept proposal,502:execute order,602:order feedback,702:order inner test,802:order inner test ok,902:order test,982:order confirmed,992:order executed,203:support feedback,303:support execute,703:support inner test,803:support inner test ok,903:support test,983:support confirmed,993:support executed,999:closed,666:joker,996:zombie';
 
 $g_set_status_threshold = array (
-  101 => 40,
-  102 => 25,
-  103 => 40,
-  201 => 40,
-  301 => 25,
-  701 => 40,
-  801 => 40,
-  901 => 40,
-  981 => 25,
-  991 => 40,
-  202 => 70,
-  302 => 25,
-  402 => 25,
-  502 => 25,
-  602 => 40,
-  702 => 40,
-  703 => 40,
-  802 => 40,
-  803 => 40,
-  902 => 40,
-  982 => 25,
-  992 => 40,
-  203 => 40,
-  303 => 25,
-  903 => 40,
-  983 => 25,
-  993 => 40,
-  996 => 70,
-  999 => 40,
-  666 => 70,
+	101 => 40,
+	102 => 25,
+	103 => 40,
+	201 => 40,
+	301 => 25,
+	701 => 40,
+	801 => 40,
+	901 => 40,
+	981 => 25,
+	991 => 40,
+	202 => 70,
+	302 => 25,
+	402 => 25,
+	502 => 25,
+	602 => 40,
+	702 => 40,
+	703 => 40,
+	802 => 40,
+	803 => 40,
+	902 => 40,
+	982 => 25,
+	992 => 40,
+	203 => 40,
+	303 => 25,
+	903 => 40,
+	983 => 25,
+	993 => 40,
+	996 => 70,
+	999 => 40,
+	666 => 70,
 );
 
 $g_default_notify_flags = array(
-  'reporter' => ON,
-  'handler' => OFF,
-  'monitor' => ON,
-  'bugnotes' => OFF,
-  //'threshold_min' => REPORTER,
-  //'threshold_max' => REPORTER,
+	'reporter' => ON,
+	'handler' => OFF,
+	'monitor' => ON,
+	'bugnotes' => OFF,
+	//'threshold_min' => REPORTER,
+	//'threshold_max' => REPORTER,
 );
 if (! $g_notify_flags ) $g_notify_flags = $g_default_notify_flags;
 $g_notify_flags = array_merge_recursive( $g_notify_flags, array (
-  'reopened' =>
-  array (
-    'reporter' => ON,
-    'handler' => ON,
-    'monitor' => ON,
-  ),
-  'deleted' =>
-  array (
-    'reporter' => ON,
-    'handler' => ON,
-    'monitor' => ON,
-  ),
-  'bugnote' =>
-  array (
-    'reporter' => ON,
-    'monitor' => ON,
-    'handler' => ON,
-    //'threshold_max' => REPORTER,
-  ),
+	'reopened' =>
+	array (
+		'reporter' => ON,
+		'handler' => ON,
+		'monitor' => ON,
+	),
+	'deleted' =>
+	array (
+		'reporter' => ON,
+		'handler' => ON,
+		'monitor' => ON,
+	),
+	'bugnote' =>
+	array (
+		'reporter' => ON,
+		'monitor' => ON,
+		'handler' => ON,
+		//'threshold_max' => REPORTER,
+	),
 ) );
 
 $g_auto_set_status_to_assigned = OFF;
-$g_bug_reopen_resolution = 101;  //new error
+$g_bug_reopen_resolution = 101;	//new error
 
 $g_allow_reporter_reopen = OFF;
 $g_allow_reporter_close = ON;
@@ -171,36 +413,36 @@ $g_bug_status_modulo = 10;
 $g_bug_status_modulo_permanent = ON;
 
 $g_status_colors = array(
-  'new' => '#FF9999',
-  'new error' => '#FFBBBB',
-  'new order' => '#FFBBBB',
-  'new support' => '#FFBBBB',
-  'error feedback' => '#FDA7FF',
-  'error execute' => '#ABEDEC',
-  'error inner test' => '#CAFD8A',
-  'error inner test ok' => '#9CE964',
-  'error test' => '#F9FFA7',
-  'error confirmed' => '#7CC944',
-  'fixed' => '#D9D9D9',
-  'proposal' => '#F9FFA7',
-  'ask proposal' => '#FFED75',
-  'accept proposal' => '#ABEDEC',
-  'execute order' => '#ABEDEC',
-  'order feedback' => '#FDA7FF',
-  'order inner test' => '#CAFD8A',
-  'order inner test ok' => '#9CE964',
-  'order test' => '#F9FFA7',
-  'order confirmed' => '#7CC944',
-  'order executed' => '#D9D9D9',
-  'support feedback' => '#FDA7FF',
-  'support execute' => '#ABEDEC',
-  'support inner test' => '#CAFD8A',
-  'support inner test ok' => '#9CE964',
-  'support test' => '#F9FFA7',
-  'support confirmed' => '#7CC944',
-  'support executed' => '#D9D9D9',
-  'closed' => '#FFFFFF',
-  'joker' => '#B9B9B9'
+	'new' => '#FF9999',
+	'new error' => '#FFBBBB',
+	'new order' => '#FFBBBB',
+	'new support' => '#FFBBBB',
+	'error feedback' => '#FDA7FF',
+	'error execute' => '#ABEDEC',
+	'error inner test' => '#CAFD8A',
+	'error inner test ok' => '#9CE964',
+	'error test' => '#F9FFA7',
+	'error confirmed' => '#7CC944',
+	'fixed' => '#D9D9D9',
+	'proposal' => '#F9FFA7',
+	'ask proposal' => '#FFED75',
+	'accept proposal' => '#ABEDEC',
+	'execute order' => '#ABEDEC',
+	'order feedback' => '#FDA7FF',
+	'order inner test' => '#CAFD8A',
+	'order inner test ok' => '#9CE964',
+	'order test' => '#F9FFA7',
+	'order confirmed' => '#7CC944',
+	'order executed' => '#D9D9D9',
+	'support feedback' => '#FDA7FF',
+	'support execute' => '#ABEDEC',
+	'support inner test' => '#CAFD8A',
+	'support inner test ok' => '#9CE964',
+	'support test' => '#F9FFA7',
+	'support confirmed' => '#7CC944',
+	'support executed' => '#D9D9D9',
+	'closed' => '#FFFFFF',
+	'joker' => '#B9B9B9'
 );
 
 define('EXECUTED', 99);
@@ -211,3 +453,7 @@ if ( SYS_COMPANY === 'waberer' ) {
 	$g_severity_enum_string	= '10:feature,50:error';
 	$g_priority_enum_string	= '30:normal,40:high,60:immediate';
 }
+ */
+
+//
+// vim: set noet:
--- config/custom_strings_inc.php
+++ config/custom_strings_inc.php
@@ -1,10 +1,47 @@
 <?php
-if( !defined('JOKER') ) require('config_statuses_inc.php');
-$s_status_enum_string = '10:új,101:új hiba,102:új megrendelés,103:új támogatás,301:hiba javítás,201:hiba kérdés,701:hiba belső teszten,801:hiba belső teszt ok,901:hibajavítás teszten,981:hibajavítás élesre tehető,991:hibajavítás élesen,202:ajánlat,302:ajánlat kérés,402:ajánlat elfogadva,502:megrendelés végrehajtása,602:megrendelés kérdés,702:megrendelés belső teszten,802:megrendelés belső teszt ok,902:megrendelés tesztre átadva,982:megrendelés élesre tehető,992:megrendelés élesen,203:támogatás kérdés,303:támogatás folyamatban,903:támogatás tesztre átadva,703:támogatás belső teszten,803:támogatás belső teszt ok,983:támogatás élesre tehető,993:támogatás élesen,999:lezárva,'.JOKER.':joker,996:zombi';
+require_once('config_statuses_inc.php');
 
+$s_my_view_title_vv = 'Verzióváltók';
+
+//$s_status_enum_string = '10:új,101:új hiba,102:új megrendelés,103:új támogatás,301:hiba javítás,201:hiba kérdés,701:hiba belső teszten,801:hiba belső teszt ok,901:hibajavítás teszten,981:hibajavítás élesre tehető,991:hibajavítás élesen,202:ajánlat,302:ajánlat kérés,402:ajánlat elfogadva,502:megrendelés végrehajtása,602:megrendelés kérdés,702:megrendelés belső teszten,802:megrendelés belső teszt ok,902:megrendelés tesztre átadva,982:megrendelés élesre tehető,992:megrendelés élesen,203:támogatás kérdés,303:támogatás folyamatban,903:támogatás tesztre átadva,703:támogatás belső teszten,803:támogatás belső teszt ok,983:támogatás élesre tehető,993:támogatás élesen,999:lezárva,'.JOKER.':joker,996:zombi';
+$s_status_enum_string = '10:új,20:kérdés,25:ajánlat kérés;30:ajánlat,40:elfogadva,50:folyamatban,60:teszt,70:teszt&nbsp;ok,80:átadva,90:lezárva';
+
+
+$s_resolution_enum_string = '10:nyitott,20:kijavítva,21:storno,29:végrehajtva,30:újranyitva,40:reprodukálhatatlan,50:javíthatatlan,60:másolat,70:nem&nbsp;kell változtatni,80:felfüggesztve,90:nem lesz kijavítva';
+
+$s_new_bug_title = 'új';
+$s_new_bug_button = 'Új';
+$s_feedback_bug_bug_title = 'kérdés';
+$s_feedback_bug_button = 'Kérdés';
+$s_acknowledged_bug_title = 'elfogadva';
+$s_acknowledged_bug_button = 'Elfogadás';
+$s_assigned_bug_title = 'folyamatban';
+$s_assigned_bug_button = 'Hozzárendelés';
+$s_ask_proposal_bug_title = 'ajánlat kérés';
+$s_ask_proposal_bug_button = 'Ajánlat kérés';
+$s_proposal_bug_title = 'ajánlat';
+$s_proposal_bug_button = 'Ajánlat adása';
+$s_test_bug_title = 'teszten';
+$s_test_bug_button = 'Tesztre átadás';
+$s_test_ok_bug_title = 'teszt OK';
+$s_test_ok_bug_button = 'Megfelelelt';
+$s_resolved_bug_title = 'átadva';
+$s_resolved_bug_button = 'Átadás';
+$s_closed_bug_title = 'lezárva';
+$s_closed_bug_button = 'Lezárás';
+
+$s_resolution_enum_string = '10:nyitott,20:kijavítva,21:storno,29:végrehajtva,30:újranyitva,40:reprodukálhatatlan,50:javíthatatlan,60:másolat,70:nem&nbsp;kell változtatni,80:felfüggesztve,90:nem lesz kijavítv';
+
+$s_severity_enum_string = '10:kérés,50:hiba';
+
+$s_priority_enum_string	= '30:normál,40:magas,60:azonnali';
+
+$s_projection_enum_string = '10:támogatás,20:hiba,50:megrendelés';
+$s_projection = 'Besorolás';
 
 //$g_status_enum_string = '10:new,101:new error,102:new order,103:new support,201:error feedback,301:error execute,701:error inner test,801:error inner test ok,901:error test,991:fixed,202:proposal,302:ask proposal,402:accept proposal,502:execute order,602:order feedback,702:order inner test,802:order inner test ok,902:order test,992:order executed,203:support feedback,303:support execute,903:support test,993:support executed,999:closed';
 
+/*
 $s_new_error_bug_title = 'új hiba';
 $s_new_error_bug_button = 'hiba bejelentése';
 $s_error_feedback_bug_title = 'hiba kérdés';
@@ -72,6 +109,6 @@ if ( SYS_COMPANY === 'waberer' ) {
 }
 
 //$MANTIS_ERROR[ERROR_BUG_VALIDATE_FAILURE] = 'A bejelentés hibás (minden kötelező mező kitöltött?)';
+*/
 
-$s_my_view_title_vv = 'Verzióváltók';
 ?>
