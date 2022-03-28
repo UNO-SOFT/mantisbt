e465fb8ed7baa22a591949ccaee86d23b7d85d3e
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2022-03-28 10:50:41 +0200

    unosoft: separate fields

--- config/config_inc.php
+++ config/config_inc.php
@@ -154,80 +154,173 @@ $g_bugnote_user_delete_threshold = DEVELOPER;
 
 $g_show_product_version = ON;
 $g_show_version_dates_threshold = REPORTER;
-$g_view_issues_page_columns[] = 'target_version';
-$g_view_issues_page_columns[] = 'fixed_in_version';
-$g_print_issues_page_columns[] = 'target_version';
-$g_print_issues_page_columns[] = 'fixed_in_version';
 
-$g_bug_report_page_fields = array(
-	'additional_info',
-	'attachments',
-	'category_id',
-	'due_date',
-	'handler',
-	'priority',
-	'product_version',
-	'reproducibility',
-	'severity',
-	#'steps_to_reproduce',
-	'tags',
-	'view_state',
-);
+if( SYS_COMPANY == 'unosoft' ) {
+    $g_view_issues_page_columns = array(
+        'selection', 'edit', 
+        //'priority', 
+        'id', 
+        //'sponsorship_total',
+        'bugnotes_count',
+        //'attachment_count',
+        //'category_id', 
+        'severity', 
+        'status',
+        'last_updated', 'summary'
+    );
 
-$g_bug_view_page_fields = array(
-	'additional_info',
-	'attachments',
-	'category_id',
-	'date_submitted',
-	'description',
-	'due_date',
-	'eta',
-	'fixed_in_version',
-	'handler',
-	'id',
-	'last_updated',
-	'priority',
-	'product_version',
-	'project',
-	'projection',
-	'reporter',
-	'reproducibility',
-	'resolution',
-	'severity',
-	'status',
-	'steps_to_reproduce',
-	'summary',
-	'tags',
-	'target_version',
-	'view_state',
-);
+    $g_print_issues_page_columns = array(
+        'selection', 
+        //'priority', 
+        'id', 'sponsorship_total', 'bugnotes_count',
+        //'attachment_count', 'category_id', 
+        'severity', 'status', 'last_updated',
+        'summary'
+    );
 
-$g_bug_update_page_fields = array(
-	'additional_info',
-	'category_id',
-	'date_submitted',
-	'description',
-	'due_date',
-	'eta',
-	'fixed_in_version',
-	'handler',
-	'id',
-	'last_updated',
-	'priority',
-	'product_version',
-	'project',
-	'projection',
-	'reporter',
-	'reproducibility',
-	'resolution',
-	'severity',
-	'status',
-	'steps_to_reproduce',
-	'summary',
-	'target_version',
-	'view_state',
-);
+    $g_bug_report_page_fields = array(
+        'additional_info',
+        'attachments',
+        //'category_id',
+        'due_date',
+        'handler',
+        //'priority',
+        //'product_version',
+        //'reproducibility',
+        'severity',
+        #'steps_to_reproduce',
+        'tags',
+        'view_state',
+    );
 
+    $g_bug_view_page_fields = array(
+        'additional_info',
+        'attachments',
+        //'category_id',
+        'date_submitted',
+        'description',
+        'due_date',
+        //'eta',
+        //'fixed_in_version',
+        'handler',
+        'id',
+        'last_updated',
+        //'priority',
+        //'product_version',
+        'project',
+        //'projection',
+        'reporter',
+        //'reproducibility',
+        'resolution',
+        'severity',
+        'status',
+        'steps_to_reproduce',
+        'summary',
+        'tags',
+        //'target_version',
+        'view_state',
+    );
+
+    $g_bug_update_page_fields = array(
+        'additional_info',
+        //'category_id',
+        'date_submitted',
+        'description',
+        'due_date',
+        //'eta',
+        //'fixed_in_version',
+        'handler',
+        'id',
+        'last_updated',
+        //'priority',
+        //'product_version',
+        'project',
+        //'projection',
+        'reporter',
+        //'reproducibility',
+        'resolution',
+        'severity',
+        'status',
+        'steps_to_reproduce',
+        'summary',
+        //'target_version',
+        'view_state',
+    );
+} else {
+    $g_view_issues_page_columns[] = 'target_version';
+    $g_view_issues_page_columns[] = 'fixed_in_version';
+    $g_print_issues_page_columns[] = 'target_version';
+    $g_print_issues_page_columns[] = 'fixed_in_version';
+
+    $g_bug_report_page_fields = array(
+        'additional_info',
+        'attachments',
+        'category_id',
+        'due_date',
+        'handler',
+        'priority',
+        'product_version',
+        'reproducibility',
+        'severity',
+        #'steps_to_reproduce',
+        'tags',
+        'view_state',
+    );
+
+    $g_bug_view_page_fields = array(
+        'additional_info',
+        'attachments',
+        'category_id',
+        'date_submitted',
+        'description',
+        'due_date',
+        'eta',
+        'fixed_in_version',
+        'handler',
+        'id',
+        'last_updated',
+        'priority',
+        'product_version',
+        'project',
+        'projection',
+        'reporter',
+        'reproducibility',
+        'resolution',
+        'severity',
+        'status',
+        'steps_to_reproduce',
+        'summary',
+        'tags',
+        'target_version',
+        'view_state',
+    );
+
+    $g_bug_update_page_fields = array(
+        'additional_info',
+        'category_id',
+        'date_submitted',
+        'description',
+        'due_date',
+        'eta',
+        'fixed_in_version',
+        'handler',
+        'id',
+        'last_updated',
+        'priority',
+        'product_version',
+        'project',
+        'projection',
+        'reporter',
+        'reproducibility',
+        'resolution',
+        'severity',
+        'status',
+        'steps_to_reproduce',
+        'summary',
+        'target_version',
+        'view_state',
+    );
+}
 
 $g_plugin_FileDistribution_url = '/static';
 $g_plugin_FileDistribution_path = '/var/local/mantis/'.SYS_FLAVOR
@@ -274,8 +367,7 @@ if( SYS_COMPANY == 'pcs' || SYS_COMPANY == 'pp' ) {
 $g_backward_year_count = 1;
 $g_forward_year_count = 1;
 
-if( SYS_FLAVOR.'/'.SYS_COMPANY != 'dev/unosoft' )
-  $g_path = preg_replace('/^http:/', 'https:', $g_path);
+$g_path = preg_replace('/^http:/', 'https:', $g_path);
 
 $g_min_refresh_delay	= 1;
 $g_default_refresh_delay				= 10;
