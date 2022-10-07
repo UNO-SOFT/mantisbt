c5e80d9dd12dc89d9fcdd45e2c814529eb728c4c
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2022-10-07 13:12:07 +0200

    fix typo

--- db/report_valaszido.sql
+++ db/report_valaszido.sql
@@ -5,7 +5,7 @@ SELECT A.id AS "mantis", A.summary AS "leírás",
        munkaoraban(A.date_submitted, A.reakcio) AS "reakcióidő",
        A.sla AS "SLA",
        (A.sla > munkaoraban(A.date_submitted, A.reakcio)) AS "megfelelő"
-  FROM (SELECT A.id, A.summary, A.priority, A.summary,
+  FROM (SELECT A.id, A.summary, A.priority, 
                to_timestamp(A.date_submitted) AS date_submitted,
                uno_reakcio(A.id) AS reakcio,
                uno_sla(A.priority::smallint, 0::smallint) AS sla
