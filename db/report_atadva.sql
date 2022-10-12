f019d70fd6b8a7b18de3fed4201e94b39124f84b
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2022-10-12 08:52:03 +0200

    db: szoveges kiertekeles

--- db/report_atadva.sql
+++ db/report_atadva.sql
@@ -1,14 +1,15 @@
-SELECT A.id AS "mantis", A.summary AS "leÃ­rÃ¡s",
-       CASE priority WHEN 30 THEN 'NormÃ¡l' WHEN 40 THEN 'KÃ¶zepesen magas' when 50 then 'SÃ¼rgÅ‘s' ELSE 'Kritikus' END AS "prioritÃ¡s",
-       A.date_submitted AS "bejelentÃ©s",
-       fejlesztonel_munkaora(A.id, A.date_submitted, COALESCE(A.atadas, localtimestamp)) AS "Ã¡tadÃ¡s",
+SELECT A.id AS "mantis", A.summary AS "leírás",
+       CASE priority WHEN 30 THEN 'Normál' WHEN 40 THEN 'Közepesen magas' when 50 then 'Sürgõs' ELSE 'Kritikus' END AS "prioritás",
+       A.date_submitted AS "bejelentés",
+       fejlesztonel_munkaora(A.id, A.date_submitted, COALESCE(A.atadas, localtimestamp)) AS "átadás",
        A.sla AS "SLA",
-       (A.sla > fejlesztonel_munkaora(A.id, A.date_submitted, COALESCE(A.atadas, localtimestamp))) AS "megfelelÅ‘"
+       CASE WHEN A.sla > fejlesztonel_munkaora(A.id, A.date_submitted, COALESCE(A.atadas, localtimestamp)) THEN 'Rendben' ELSE 'Probléma' END AS "kiértékelés"
   FROM (SELECT A.id, A.priority, A.summary,
                to_timestamp(A.date_submitted) AS date_submitted,
                uno_atadas(A.id)  AS atadas,
                uno_sla(A.priority::smallint, 2::smallint) AS sla
           FROM mantis_bug_table A
           WHERE date_submitted > 1656626400 --2022.07.01
-  ) A;
+  ) A
+  ORDER BY 1;
 --vim:se et shiftwidth=2 fenc=utf-8:
--- db/report_valaszido.sql
+++ db/report_valaszido.sql
@@ -1,16 +1,17 @@
-SELECT A.id AS "mantis", A.summary AS "leÃ­rÃ¡s",
-       CASE priority WHEN 30 THEN 'NormÃ¡l' WHEN 40 THEN 'KÃ¶zepesen magas' when 50 then 'SÃ¼rgÅ‘s' ELSE 'Kritikus' END AS "prioritÃ¡s",
-       A.date_submitted AS "bejelentÃ©s",
-       A.reakcio AS "vÃ¡lasz",
-       munkaoraban(A.date_submitted, A.reakcio) AS "reakciÃ³idÅ‘",
+SELECT A.id AS "mantis", A.summary AS "leírás",
+       CASE priority WHEN 30 THEN 'Normál' WHEN 40 THEN 'Közepesen magas' when 50 then 'Sürgõs' ELSE 'Kritikus' END AS "prioritás",
+       A.date_submitted AS "bejelentés",
+       A.reakcio AS "válasz",
+       munkaoraban(A.date_submitted, A.reakcio) AS "reakcióidõ",
        A.sla AS "SLA",
-       (A.sla > munkaoraban(A.date_submitted, A.reakcio)) AS "megfelelÅ‘"
+    CASE WHEN A.sla > munkaoraban(A.date_submitted, A.reakcio) THEN 'Rendben' ELSE 'Probléma' END AS "kiértékelés"
   FROM (SELECT A.id, A.summary, A.priority, 
                to_timestamp(A.date_submitted) AS date_submitted,
                uno_reakcio(A.id) AS reakcio,
                uno_sla(A.priority::smallint, 0::smallint) AS sla
           FROM mantis_bug_table A
           WHERE date_submitted > 1656626400 --2022.07.01
-  ) A;
+  ) A
+  ORDER BY 1;
 
 --vim:se et shiftwidth=2 fenc=utf-8:
