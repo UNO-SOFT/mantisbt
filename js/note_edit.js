c1d6ba5f91fbf55c426f33f900b92316e86465d8
Gulácsi Tamás <T.Gulacsi@unosoft.hu>
2025-04-26 11:13:14 +0200

    note_edit: wait for #MD#

--- js/note_edit.js
+++ js/note_edit.js
@@ -1,19 +1,24 @@
-$(document).ready(function(){
-  function render(input) {
-    let text = input.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, "");
-		if( text.startsWith('#MD#\n') ) {
-		  return marked.parse( text.substr(5) );
-		}
-		return "";
-  }
-  ["bugnote_text", "description", "steps_to_reproduce"].forEach(function(name) {
-    const ta = document.getElementById(name);
-    if( ta.value == '#MD#' || ta.value.startsWith( '#MD#\n' ) ) {
+$(document).ready(function() {
+  function mayInitMDE(ta) {
+    if( ta.value == "#MD#" || ta.value.startsWith( '#MD#\n' ) ) {
       const easyMDE = new EasyMDE({
         element: ta,
         hideIcons: ["image", "upload-image"],
         spellChecker: false,
       });
+      return true;
+    }
+    return false;
+  }
+  ["bugnote_text", "description", "steps_to_reproduce"].forEach(function(name) {
+    const ta = document.getElementById(name);
+    if( !mayInitMDE(ta) ){
+      function mayInit() {
+        if( mayInitMDE(ta) ) {
+          ta.removeEventListener("keyup", mayInit)
+        }
+      }
+      ta.addEventListener("keyup", mayInit);
     }
   });
 });
